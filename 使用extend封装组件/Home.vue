c2138424e765c7274c50bfaafdf9883fc49b38c1
zdl958112850 <958112850@qq.com>
2019-09-04 22:48:32 +0800

    extend封装vue组件

---
+++ 使用extend封装组件/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrap" v-if="show">
+        <div>{{text}}</div>
+        <div>{{temp1}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            temp1 : "你好vbyzc"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .wrap{
+        color:#fff;
+        background-color: rgba(0,0,0,0.8);
+        padding: 10px;
+        position: fixed;
+        top:50%;left: 50%;
+        border-radius: 10px;;
+    }
+</style>
