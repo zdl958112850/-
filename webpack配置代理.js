78c8b06c295934a352719221662fd1332aa7e9f8
zdl958112850 <958112850@qq.com>
2019-09-06 17:08:06 +0800

    代理配置

---
+++ webpack配置代理.js
@@ -0,0 +1,19 @@
+ proxyTable: {
+
+      '/api': {
+
+            target: 'http://172.00.61.243:8082', // 你接口的域名  http://172.00.61.243:8082
+
+            //secure: false,      // 如果是https接口，需要配置这个参数
+
+            changeOrigin: true,     // 如果接口跨域，需要进行这个参数配置
+
+            pathRewrite: {
+
+              '^/api': ''
+
+            }
+
+        }
+
+  　},
